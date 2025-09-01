c0a162f1ac41100588d9d136df31c99beb5e0dda
Anthony-19 <anthonyeze248008@gmail.com>
2025-09-01 08:08:48 +0100

    Updaated the read me file

---
+++ src/app/3d-models/layout.jsx
@@ -0,0 +1,40 @@
+
+import Link from "next/link";
+import Image from "next/image"
+import "./globals.css";
+
+
+export default function ModelLayout({ children }) {
+  return (
+        <section className="container">
+          <div className="print-forge-container">
+            <header className="header">
+              <Link href="/" className="home-link">
+               <Image
+                className="print-forge-logo"
+                src="/printforge-logo.svg"
+                alt="Print Forge Logo"
+                width={150}
+                height={50}
+                priority
+              />
+              </Link>
+             
+              <nav className="nav">
+                <ul className="nav-list">
+                  <li className="nav-item">
+                    <Link href="/3d-models" className="nav-link">3D MODELS</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link href="/about" className="nav-link about">ABOUT</Link>
+                  </li>
+                </ul>
+              </nav>
+            </header>
+            {children}
+          </div>
+        </section>
+       
+   
+  );
+}
