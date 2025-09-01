08aa56ec0e274cc0856a6761900d70937cc4eeda
Anthony-19 <anthonyeze248008@gmail.com>
2025-09-01 18:52:39 +0100

    Updated the styling for my 3d model

--- src/app/3d-models/layout.jsx
+++ src/app/3d-models/layout.jsx
@@ -1,42 +1,43 @@
 
 import Link from "next/link";
+import styles from './page.module.css'
 
 
 export default function ModelLayout({ children }) {
   return (
-        <section className="modelLayoutContainer">
-          <div className="ModelContainer">
-            <section className="categoryList">
-               <ul>
-                <li>
-                    <Link href="/3d-models/category1">All</Link>
+        <section className={styles.modelLayoutContainer}>
+          <div className={styles.modelContainer}>
+            <section className={styles.categoryListContainer}>
+               <ul className={styles.categoryList}>
+                <li >
+                    <Link href="/3d-models/category1" className={styles.categoryLink}>All</Link>
                 </li>
                 <li>
-                    <Link href="/3d-models/category2">3D PRINTER</Link>
+                    <Link href="/3d-models/category2" className={styles.categoryLink}>3D PRINTER</Link>
                 </li>
                 <li>
-                    <Link href="/3d-models/category3">ART</Link>
+                    <Link href="/3d-models/category3" className={styles.categoryLink}>ART</Link>
                 </li>
                  <li>
-                    <Link href="/3d-models/category3">EDUCATION</Link>
+                    <Link href="/3d-models/category3" className={styles.categoryLink}>EDUCATION</Link>
                 </li>
                  <li>
-                    <Link href="/3d-models/category3">FASHION</Link>
+                    <Link href="/3d-models/category3" className={styles.categoryLink}>FASHION</Link>
                 </li>
                  <li>
-                    <Link href="/3d-models/category3">HOBBY & DIY</Link>
+                    <Link href="/3d-models/category3" className={styles.categoryLink}>HOBBY & DIY</Link>
                 </li>
                 <li>
-                    <Link href="/3d-models/category3">HOUSEHOLD</Link>
+                    <Link href="/3d-models/category3" className={styles.categoryLink}>HOUSEHOLD</Link>
                 </li>
                 <li>
-                    <Link href="/3d-models/category3">MANIATURES</Link>
+                    <Link href="/3d-models/category3" className={styles.categoryLink}>MANIATURES</Link>
                 </li>
                 <li>
-                    <Link href="/3d-models/category3">TOOLS</Link>
+                    <Link href="/3d-models/category3" className={styles.categoryLink}>TOOLS</Link>
                 </li>
                  <li>
-                    <Link href="/3d-models/category3">TOYS & GAMES</Link>
+                    <Link href="/3d-models/category3" className={styles.categoryLink}>TOYS & GAMES</Link>
                 </li>
                </ul>
             </section>
--- src/app/3d-models/page.jsx
+++ src/app/3d-models/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
 import Form from 'next/form'
+import styles from './page.module.css'
 
 function ModelPage() {
   return (
-    <div style={{marginTop: '4rem',
-        fontSize: '1rem',
-    }} className='categoriesSection'>
+    <div style={{
+        fontSize: '1rem'
+    }} className={styles.categoriesSection}>
         <section className='categoryHeader'>
             <h2>3D Models</h2>
             <Form action="/3d-models/">
@@ -15,10 +17,319 @@ function ModelPage() {
                 placeholder="search for a model" />
             </Form>
         </section>
-        <section>
+        <section className='categoryCardContainer'>
+            <div className="categoryCard">
+                <Image 
+                    src="/about-hero-image.svg"
+                    alt="3D Model"
+                    className='cardImage'
+                    width={300}
+                    height={300}
+                    priority
+                />
+                <div className="categoryCardBody">
+                    <h3 className='cardTitle'>Printer Upgrade Kit</h3>
+                    <p className='cardDescription'>Enhancement parts for 3D printer performance</p>
+                    <button className='cardCategory'>3D-Printer</button>
+                    <div className='reactionContainer'>
+                        <Image 
+                            src="/love-reaction.svg"
+                            alt="Reaction"
+                            width={100}
+                            height={100}
+                        />
+                        <p className='numberReaction'>300</p>
+                    </div>
+                </div>
+            </div>
 
+            <div className="categoryCard">
+                <Image 
+                    src="/about-hero-image.svg"
+                    alt="3D Model"
+                    className='cardImage'
+                    width={300}
+                    height={300}
+                    priority
+                />
+                <div className="categoryCardBody">
+                    <h3 className='cardTitle'>Printer Upgrade Kit</h3>
+                    <p className='cardDescription'>Enhancement parts for 3D printer performance</p>
+                    <button className='cardCategory'>3D-Printer</button>
+                    <div className='reactionContainer'>
+                        <Image 
+                            src="/love-reaction.svg"
+                            alt="Reaction"
+                            width={100}
+                            height={100}
+                        />
+                        <p className='numberReaction'>300</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="categoryCard">
+                <Image 
+                    src="/about-hero-image.svg"
+                    alt="3D Model"
+                    className='cardImage'
+                    width={300}
+                    height={300}
+                    priority
+                />
+                <div className="categoryCardBody">
+                    <h3 className='cardTitle'>Printer Upgrade Kit</h3>
+                    <p className='cardDescription'>Enhancement parts for 3D printer performance</p>
+                    <button className='cardCategory'>3D-Printer</button>
+                    <div className='reactionContainer'>
+                        <Image 
+                            src="/love-reaction.svg"
+                            alt="Reaction"
+                            width={100}
+                            height={100}
+                        />
+                        <p className='numberReaction'>300</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="categoryCard">
+                <Image 
+                    src="/about-hero-image.svg"
+                    alt="3D Model"
+                    className='cardImage'
+                    width={300}
+                    height={300}
+                    priority
+                />
+                <div className="categoryCardBody">
+                    <h3 className='cardTitle'>Printer Upgrade Kit</h3>
+                    <p className='cardDescription'>Enhancement parts for 3D printer performance</p>
+                    <button className='cardCategory'>3D-Printer</button>
+                    <div className='reactionContainer'>
+                        <Image 
+                            src="/love-reaction.svg"
+                            alt="Reaction"
+                            width={100}
+                            height={100}
+                        />
+                        <p className='numberReaction'>300</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="categoryCard">
+                <Image 
+                    src="/about-hero-image.svg"
+                    alt="3D Model"
+                    className='cardImage'
+                    width={300}
+                    height={300}
+                    priority
+                />
+                <div className="categoryCardBody">
+                    <h3 className='cardTitle'>Printer Upgrade Kit</h3>
+                    <p className='cardDescription'>Enhancement parts for 3D printer performance</p>
+                    <button className='cardCategory'>3D-Printer</button>
+                    <div className='reactionContainer'>
+                        <Image 
+                            src="/love-reaction.svg"
+                            alt="Reaction"
+                            width={100}
+                            height={100}
+                        />
+                        <p className='numberReaction'>300</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="categoryCard">
+                <Image 
+                    src="/about-hero-image.svg"
+                    alt="3D Model"
+                    className='cardImage'
+                    width={300}
+                    height={300}
+                    priority
+                />
+                <div className="categoryCardBody">
+                    <h3 className='cardTitle'>Printer Upgrade Kit</h3>
+                    <p className='cardDescription'>Enhancement parts for 3D printer performance</p>
+                    <button className='cardCategory'>3D-Printer</button>
+                    <div className='reactionContainer'>
+                        <Image 
+                            src="/love-reaction.svg"
+                            alt="Reaction"
+                            width={100}
+                            height={100}
+                        />
+                        <p className='numberReaction'>300</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="categoryCard">
+                <Image 
+                    src="/about-hero-image.svg"
+                    alt="3D Model"
+                    className='cardImage'
+                    width={300}
+                    height={300}
+                    priority
+                />
+                <div className="categoryCardBody">
+                    <h3 className='cardTitle'>Printer Upgrade Kit</h3>
+                    <p className='cardDescription'>Enhancement parts for 3D printer performance</p>
+                    <button className='cardCategory'>3D-Printer</button>
+                    <div className='reactionContainer'>
+                        <Image 
+                            src="/love-reaction.svg"
+                            alt="Reaction"
+                            width={100}
+                            height={100}
+                        />
+                        <p className='numberReaction'>300</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="categoryCard">
+                <Image 
+                    src="/about-hero-image.svg"
+                    alt="3D Model"
+                    className='cardImage'
+                    width={300}
+                    height={300}
+                    priority
+                />
+                <div className="categoryCardBody">
+                    <h3 className='cardTitle'>Printer Upgrade Kit</h3>
+                    <p className='cardDescription'>Enhancement parts for 3D printer performance</p>
+                    <button className='cardCategory'>3D-Printer</button>
+                    <div className='reactionContainer'>
+                        <Image 
+                            src="/love-reaction.svg"
+                            alt="Reaction"
+                            width={100}
+                            height={100}
+                        />
+                        <p className='numberReaction'>300</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="categoryCard">
+                <Image 
+                    src="/about-hero-image.svg"
+                    alt="3D Model"
+                    className='cardImage'
+                    width={300}
+                    height={300}
+                    priority
+                />
+                <div className="categoryCardBody">
+                    <h3 className='cardTitle'>Printer Upgrade Kit</h3>
+                    <p className='cardDescription'>Enhancement parts for 3D printer performance</p>
+                    <button className='cardCategory'>3D-Printer</button>
+                    <div className='reactionContainer'>
+                        <Image 
+                            src="/love-reaction.svg"
+                            alt="Reaction"
+                            width={100}
+                            height={100}
+                        />
+                        <p className='numberReaction'>300</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="categoryCard">
+                <Image 
+                    src="/about-hero-image.svg"
+                    alt="3D Model"
+                    className='cardImage'
+                    width={300}
+                    height={300}
+                    priority
+                />
+                <div className="categoryCardBody">
+                    <h3 className='cardTitle'>Printer Upgrade Kit</h3>
+                    <p className='cardDescription'>Enhancement parts for 3D printer performance</p>
+                    <button className='cardCategory'>3D-Printer</button>
+                    <div className='reactionContainer'>
+                        <Image 
+                            src="/love-reaction.svg"
+                            alt="Reaction"
+                            width={100}
+                            height={100}
+                        />
+                        <p className='numberReaction'>300</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className="categoryCard">
+                <Image 
+                    src="/about-hero-image.svg"
+                    alt="3D Model"
+                    className='cardImage'
+                    width={300}
+                    height={300}
+                    priority
+                />
+                <div className="categoryCardBody">
+                    <h3 className='cardTitle'>Printer Upgrade Kit</h3>
+                    <p className='cardDescription'>Enhancement parts for 3D printer performance</p>
+                    <button className='cardCategory'>3D-Printer</button>
+                    <div className='reactionContainer'>
+                        <Image 
+                            src="/love-reaction.svg"
+                            alt="Reaction"
+                            width={100}
+                            height={100}
+                        />
+                        <p className='numberReaction'>300</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className="categoryCard">
+                <Image 
+                    src="/about-hero-image.svg"
+                    alt="3D Model"
+                    className='cardImage'
+                    width={300}
+                    height={300}
+                    priority
+                />
+                <div className="categoryCardBody">
+                    <h3 className='cardTitle'>Printer Upgrade Kit</h3>
+                    <p className='cardDescription'>Enhancement parts for 3D printer performance</p>
+                    <button className='cardCategory'>3D-Printer</button>
+                    <div className='reactionContainer'>
+                        <Image 
+                            src="/love-reaction.svg"
+                            alt="Reaction"
+                            width={100}
+                            height={100}
+                        />
+                        <p className='numberReaction'>300</p>
+                    </div>
+                </div>
+            </div>
         </section>
-      <h1>Hello world Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi velit temporibus nisi iste error id impedit minima sint aut, cupiditate laudantium mollitia repellendus non ab deleniti consectetur deserunt veritatis! Tempore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, porro? Beatae fugit dolorum autem excepturi voluptate sed id dolores porro eligendi, hic unde in ea, repudiandae aliquam esse quibusdam! Vero.</h1>
+     
     </div>
   )
 }
